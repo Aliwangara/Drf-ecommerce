6ddebddfe8fac36539d79cdf5c95e10b4eedbccc
Aliwangara <aliwangara63@gmail.com>
2024-03-19 22:49:29 +0300

    Registered new Urls
    I registered new Urls for accessibility of the data in the database

--- Main_shop/Main_shop/urls.py
+++ Main_shop/Main_shop/urls.py
@@ -3,13 +3,21 @@
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.routers import DefaultRouter
 from Main_shop.product import views
 
 router =DefaultRouter()
-router.register(r'Category', views.CategoryView)
+router.register(r'Category', views.CategoryView),
+router.register(r'Brand', views.BrandView),
+router.register(r'Product', views.ProductView),
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include(router.urls))
+    path('api/',include(router.urls)),
+    path("api/schema", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/docs",SpectacularSwaggerView.as_view(url_name="schema") ),
+    
+    
 ]
