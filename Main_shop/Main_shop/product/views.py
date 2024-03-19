bafa39c3e8be9e342df2a939ae4df64312f9b247
Aliwangara <aliwangara63@gmail.com>
2024-03-19 22:45:56 +0300

    Made changes in the serializer
    I added foreign tables to the Product Serializer.

--- Main_shop/Main_shop/product/views.py
+++ Main_shop/Main_shop/product/views.py
@@ -2,47 +2,57 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import Category,Product,Brand
+from drf_spectacular.utils import extend_schema
 
 from .serializers import CategorySerializer, ProductSerializer,BrandSerializer
 
-
+@extend_schema(responses=CategorySerializer)
 class CategoryView(viewsets.ViewSet):
     
    queryset = Category.objects.all()
    
    def list(self,request):
        serializer = CategorySerializer(self.queryset, many=True)
-       if serializer.is_valid():
-        return Response(serializer.data)
-       else:
-           return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-           
-    
-   
        
-class ProductView(viewsets.ViewSet):
-    
-    queryset = Category.objects.all()
-    
-    def list(self,request):
-        serializer= ProductSerializer(self.queryset, many=True)
-        if serializer.is_valid():
-          return Response(serializer.data)
-        else:
-           return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-        
-        
+       return Response (serializer.data)
+   
+   
+   
+@extend_schema(responses=BrandSerializer)
 class BrandView(viewsets.ViewSet):
     
-    queryset = Brand.objects.all()
+   queryset = Brand.objects.all()
+   
+   
+   
+   def list(self,request):
+       serializer = BrandSerializer(self.queryset, many=True)
+       
+       
+       
+       return Response (serializer.data)
+   
+   
+class ProductView(viewsets.ViewSet):
     
-    def list(self, request):
-        serializer = BrandSerializer(self.queryset, many=True)  
+   queryset = Product.objects.all()
+   
+   
+   
+   def list(self,request):
+       serializer = ProductSerializer(self.queryset, many=True)
+       
+       return Response (serializer.data)
+   
+   
+
         
-        if serializer.is_valid():
-         return Response(serializer.data)
-        else:
-           return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+           
+
+        
+        
+        
+ 
+        
+        
 
